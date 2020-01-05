6f4a8d791dd444d46c9707a31efff73082daa145
arnaubennassar5@gmail.com <arnaubennassar5@gmail.com>
2020-01-05 02:15:50 +0100

    circuit working

--- circuit/main.go
+++ circuit/main.go
@@ -1,95 +1,151 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-
-	circuit "github.com/libp2p/go-libp2p-circuit"
-	swarm "github.com/libp2p/go-libp2p-swarm"
-	ma "github.com/multiformats/go-multiaddr"
 )
 
+var p host.Host
+
 func main() {
-	// Create three libp2p hosts, enable relay client capabilities on all
-	// of them.
-
-	// Tell the host to monitor for relays.
-	h1, err := libp2p.New(context.Background(), libp2p.EnableRelay(circuit.OptDiscovery))
+	// create peer
+	config := parseFlags()
+	var err error
+	p, err = libp2p.New(
+		context.Background(),
+		config.listen,
+		config.relay,
+	)
 	if err != nil {
 		panic(err)
 	}
 
-	// Tell the host to relay connections for other peers (The ability to *use*
-	// a relay vs the ability to *be* a relay)
-	h2, err := libp2p.New(context.Background(), libp2p.EnableRelay(circuit.OptHop))
-	if err != nil {
-		panic(err)
-	}
-
-	// Zero out the listen addresses for the host, so it can only communicate
-	// via p2p-circuit for our example
-	h3, err := libp2p.New(context.Background(), libp2p.ListenAddrs(), libp2p.EnableRelay())
-	if err != nil {
-		panic(err)
-	}
-
-	h2info := peer.AddrInfo{
-		ID:    h2.ID(),
-		Addrs: h2.Addrs(),
-	}
-
-	// Connect both h1 and h3 to h2, but not to each other
-	if err := h1.Connect(context.Background(), h2info); err != nil {
-		panic(err)
-	}
-	if err := h3.Connect(context.Background(), h2info); err != nil {
-		panic(err)
-	}
-
-	// Now, to test things, let's set up a protocol handler on h3
-	h3.SetStreamHandler("/cats", func(s network.Stream) {
-		fmt.Println("Meow! It worked!")
+	// listen to other peers
+	p.SetStreamHandler("/ping", func(s network.Stream) {
+		fmt.Println("________________________________________")
+		fmt.Println("Ping!")
+		printStreamInfo(s)
+		fmt.Println("________________________________________")
 		s.Close()
 	})
 
-	_, err = h1.NewStream(context.Background(), h3.ID(), "/cats")
-	if err == nil {
-		fmt.Println("Didnt actually expect to get a stream here. What happened?")
+	// App loop
+	fmt.Println("\n\n\n========================================")
+	fmt.Println("Hi!")
+	printWhoami()
+	fmt.Println("========================================")
+	fmt.Println("You can add me by running this command in another instance of this app:")
+	printAddme()
+	fmt.Println("========================================")
+	if config.noInteractive {
+		// wait for a SIGINT or SIGTERM signal
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+		fmt.Println("Received signal, shutting down...")
+
+		// shut the node down
+		if err := p.Close(); err != nil {
+			panic(err)
+		}
 		return
 	}
-	fmt.Println("Okay, no connection from h1 to h3: ", err)
-	fmt.Println("Just as we suspected")
-
-	// Creates a relay address
-	relayaddr, err := ma.NewMultiaddr("/p2p-circuit/ipfs/" + h3.ID().Pretty())
-	if err != nil {
-		panic(err)
+	printHelp()
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("________________________________________")
+		fmt.Println("Enter command:")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			panic("Error receiving input: " + err.Error())
+		}
+		cmd := parseInput(input)
+		switch cmd[0] {
+		case "help":
+			printHelp()
+		case "whoami":
+			printWhoami()
+		case "addme":
+			printAddme()
+		case "add":
+			addPeer(cmd)
+		case "ls":
+			printPeerList()
+		case "ping":
+			ping(cmd)
+		case "quit":
+			return
+		default:
+			fmt.Println("Unrecognized command")
+			printHelp()
+		}
 	}
-
-	// Since we just tried and failed to dial, the dialer system will, by default
-	// prevent us from redialing again so quickly. Since we know what we're doing, we
-	// can use this ugly hack (it's on our TODO list to make it a little cleaner)
-	// to tell the dialer "no, its okay, let's try this again"
-	h1.Network().(*swarm.Swarm).Backoff().Clear(h3.ID())
-
-	h3relayInfo := peer.AddrInfo{
-		ID:    h3.ID(),
-		Addrs: []ma.Multiaddr{relayaddr},
-	}
-	if err := h1.Connect(context.Background(), h3relayInfo); err != nil {
-		panic(err)
-	}
-
-	// Woohoo! we're connected!
-	s, err := h1.NewStream(context.Background(), h3.ID(), "/cats")
-	if err != nil {
-		fmt.Println("huh, this should have worked: ", err)
-		return
-	}
-
-	s.Read(make([]byte, 1)) // block until the handler closes the stream
 }
+
+func printHelp() {
+	fmt.Println("APP FLAGS (passed when starting the app):")
+	fmt.Println("\n-listen\t\t\tList of addrs the peer will listen to, quoted (\"\") and separated by space. Useful if behind proxy, DNS, portforwarding, ...")
+	fmt.Println("-relay\t\t\tShould this peer relay connections to other peers? Useful if you want this peer to act as a gateway. If not passed NO is assumed")
+	fmt.Println("-no_interactive\t\t\tRun without user intervention. For ping and relay purposes")
+	fmt.Println("\nAPP USAGE (once the app is running):")
+	fmt.Println("\nhelp\t\t\t Prints this message.")
+	fmt.Println("whoami\t\t\t Prints info about this peer (ID, addr).")
+	fmt.Println("addme\t\t\t Prints command to be used by others in order to find you.")
+	fmt.Println("add [peer ID] [addr]\t Adds a peer with the given ID and addr.")
+	fmt.Println("ls\t\t\t Print list of connected peers.")
+	fmt.Println("ping [peer ID] \t\t Send a ping to peer ID.")
+	fmt.Println("quit\t\t\t Stop app.")
+}
+
+func printWhoami() {
+	fmt.Println("I'm the peer:", p.ID().Pretty())
+}
+
+func printAddme() {
+	addrs := p.Addrs()
+	addrString := "[/p2p-circuit/p2p/" + p.ID().Pretty()
+	for _, v := range addrs {
+		addrString += " " + v.String()
+	}
+	addrString += "]"
+	fmt.Println("add", peer.IDHexEncode(p.ID()), addrString)
+}
+
+func addPeer(args []string) {
+	if len(args) != 3 {
+		fmt.Println("You need to specify peer ID and Addr")
+		return
+	}
+	peerInfo := parsePeerInfo(args[1], args[2])
+	if err := p.Connect(context.Background(), peerInfo); err != nil {
+		fmt.Println("Error connecting to the peer")
+		return
+	}
+	fmt.Println("Peer added:", peerInfo.ID.Pretty())
+}
+
+func printPeerList() {
+	fmt.Println(p.Peerstore().Peers())
+}
+
+func ping(args []string) {
+	s, err := p.NewStream(context.Background(), parseID(args[1]), "/ping")
+	if err != nil {
+		fmt.Println("Error stablishing connection: ", err)
+		return
+	}
+	fmt.Println("Pong!")
+	printStreamInfo(s)
+	s.Read(make([]byte, 1))
+}
+
+// TODO: import / export
---
+++ circuit/utils.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/libp2p/go-libp2p"
+	circuit "github.com/libp2p/go-libp2p-circuit"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p/config"
+	"github.com/multiformats/go-multiaddr"
+)
+
+type conf struct {
+	listen        config.Option
+	relay         config.Option
+	noInteractive bool
+}
+
+func parseFlags() *conf {
+	c := &conf{}
+	var listen string
+	var relay bool
+	flag.StringVar(&listen, "listen", "", "List of addrs the peer will listen to, separated by space")
+	flag.BoolVar(&relay, "relay", false, "Should this peer relay connections to other peers? (Useful if you want this peer to act as a gateway)")
+	flag.BoolVar(&c.noInteractive, "no-interactive", false, "Run without user intervention. For ping and relay purposes")
+
+	flag.Parse()
+
+	if relay {
+		c.relay = libp2p.EnableRelay(circuit.OptHop)
+	} else {
+		c.relay = libp2p.EnableRelay(circuit.OptDiscovery)
+	}
+	if listen == "" {
+		c.listen = libp2p.ListenAddrStrings()
+	} else {
+		addrs := strings.Split(listen, " ")
+		c.listen = libp2p.ListenAddrStrings(addrs...)
+	}
+	return c
+}
+
+func parseInput(inp string) []string {
+	noNewLine := strings.Split(inp, "\n")[0]
+	splitAddr := strings.Split(noNewLine, "[")
+	splitOpts := strings.Split(splitAddr[0], " ")
+	if 1 < len(splitAddr) {
+		return append(splitOpts[:len(splitOpts)-1], "["+splitAddr[1])
+	}
+	return splitOpts
+}
+
+func parseID(from string) peer.ID {
+	ID, err := peer.IDHexDecode(from)
+	if err != nil {
+		fmt.Println("Invalid peer ID:", err)
+	}
+	return ID
+}
+
+func getAddrList(from string) []string {
+	peerAddrString := strings.Replace(strings.Replace(from, "[", "", 1), "]", "", 1)
+	return strings.Split(peerAddrString, " ")
+}
+
+func parseAddr(from string) []multiaddr.Multiaddr {
+	peerAddrList := getAddrList(from)
+	peerAddr := make([]multiaddr.Multiaddr, len(peerAddrList))
+	for i := 0; i < len(peerAddrList); i++ {
+		addr, err := multiaddr.NewMultiaddr(peerAddrList[i])
+		if err != nil {
+			fmt.Println("Invalid peer address:", err)
+		}
+		peerAddr[i] = addr
+	}
+	return peerAddr
+}
+
+func parsePeerInfo(ID, addr string) peer.AddrInfo {
+	return peer.AddrInfo{
+		ID:    parseID(ID),
+		Addrs: parseAddr(addr),
+	}
+}
+
+func printStreamInfo(s network.Stream) {
+	fmt.Println("Message comes through:", s.Conn().RemoteMultiaddr())
+	fmt.Println("By: ", s.Conn().RemotePeer())
+}
